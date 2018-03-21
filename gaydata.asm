932296d66d9ac00e85a6accf84c1e814e5b85248
Makena Kong <mkong02@calpoly.edu>
2018-03-20 22:40:58 -0700

    are you gay quiz

---
+++ gaydata.asm
@@ -0,0 +1,29 @@
+;How gay are you?
+
+        .ORIG   x3300
+
+Q1      .STRINGZ "\nWho is your favorite Kardashian?\n 1 - Kylie\n 2 - Kim\n 3 - Kris\n"
+Q1Answ  .FILL   #1
+        .FILL   #1
+        .FILL   #1
+
+Q2      .STRINGZ "\nWhat is your favorite flavor ice cream? 1 - vanilla\n 2 - chocolate\n 3 - strawberry\n"
+Q2Answ  .FILL   #1
+        .FILL   #1
+        .FILL   #1
+
+Q3      .STRINGZ '\nWhat is your favorite subject?\n 1 - math\n 2 - english\n 3 - pe\n"
+Q3Answ  .FILL   #1
+        .FILL   #1
+        .FILL   #1
+
+Q4      .STRINGZ "\n Do you think you are gay?\n 1 - no way\n 2 - yes way\n 3 - i don't know\n"
+Q4Answ  .FILL   #1
+        .FILL   #1
+        .FILL   #1
+
+RESULT1 .STRINGZ "\n Yes - you are super gay."
+RESULT2 .STRINGZ "\n Yes - you are super gay."
+RESULT3 .STRINGZ "\n Yes - you are super gay."
+
+        .END
